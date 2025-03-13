79da598a3b7f0c0f78449809fe04161ab92c975f
Althayr Santos <althabrandao@gmail.com>
2025-03-13 09:50:36 -0300

    Updating startup script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 echo "🔄 Starting development environment setup..."
 
 ## Dev startup
-pnpm install;
-pnpm db:generate;
-pnpm db:migrate;
-pnpm dev;
+yes | pnpm install;
+# pnpm db:generate;
+# pnpm db:migrate;
+# pnpm dev;
 
-# ## Prod startup
-# pnpm build;
-# pnpm start;
+## Prod startup
+pnpm build;
+pnpm start;
 
